a42a403c4e734d61dffaf942f2496d66995e40fa
Allen Hansrisuk <ahansrisuk@email.com>
2020-07-29 16:22:23 -0400

    create basic form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Form from "./components/Form";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="pt-10  text-center mt-6 text-3xl leading-9 font-extrabold text-gray-900">
+        Search for a GitHub User
+      </h1>
+      <Form />
     </div>
   );
 }
