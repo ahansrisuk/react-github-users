f5adc29ea49bf8265476a21b3db3597f5db9fa36
Allen Hansrisuk <ahansrisuk@email.com>
2020-07-30 10:11:35 -0400

    add key

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -4,7 +4,7 @@ import Card from "./Card.js";
 const CardList = (props) => (
   <div id="container" className="mt-10">
     {props.cards.map((card) => (
-      <Card {...card} />
+      <Card {...card} key={card.props.login} />
     ))}
   </div>
 );
